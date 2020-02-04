54cf5185409b1541ab523a47fe9bba80946a63a8
Cynthia(NAPS) <sally5610@gmail.com>
2020-02-04 17:01:57 -0500

    Updated fields to be optional

--- familytree/webapp/models.py
+++ familytree/webapp/models.py
@@ -4,11 +4,11 @@ prefix_choices = tuple()
 
 
 class Name(models.Model):
-    prefix = models.CharField(max_length=7, choices=prefix_choices)
+    prefix = models.CharField(max_length=7, choices=prefix_choices, blank=True) 
     first_name = models.TextField()
-    middle_name = models.TextField()
-    last_name = models.TextField()
-    suffix = models.CharField(max_length=6)
+    middle_name = models.TextField(blank=True)
+    last_name = models.TextField(blank=True)
+    suffix = models.CharField(max_length=6, blank=True)
 
 
 class LegalName(Name):
@@ -21,31 +21,31 @@ class AlternateName(Name):
 
 class Person(models.Model):
     name = models.OneToOneField(LegalName, on_delete=models.CASCADE)
-    preferred_name = models.TextField()
-    birth_date = models.DateField()
-    death_date = models.DateField()
-    birth_location = models.TextField() #TODO: change to location class
-    death_location = models.TextField() #TODO: change to location class
-    living = models.BooleanField()
-    gender = models.CharField(max_length=100)
-    notes = models.TextField()
-    occupations = models.TextField()
-    partnerships = models.ManyToManyField('Partnership')
-    parents = models.ManyToManyField('Parent')
+    preferred_name = models.TextField(blank=True)
+    birth_date = models.DateField(null=True)
+    death_date = models.DateField(null=True)
+    birth_location = models.TextField(blank=True) #TODO: change to location class
+    death_location = models.TextField(blank=True) #TODO: change to location class
+    living = models.BooleanField(default='TRUE')
+    gender = models.CharField(max_length=100) #Should gender be optional?
+    notes = models.TextField(blank=True)
+    occupations = models.TextField(blank=True)
+    partnerships = models.ManyToManyField('Partnership', blank=True, null=True) 
+    parents = models.ManyToManyField('Parent', blank=True, null=True)
 
 
 class Partnership(models.Model):
     partner = models.ForeignKey(Person, on_delete=models.DO_NOTHING, related_name='+', null=True)
     children = models.ManyToManyField(Person, related_name='+')
-    married = models.BooleanField()
-    marriage_date = models.DateField()
-    divorced = models.BooleanField()
-    divorce_date = models.DateField()
-    notes = models.TextField()
+    married = models.BooleanField(default='FALSE')
+    marriage_date = models.DateField(null=True)
+    divorced = models.BooleanField(default='FALSE')
+    divorce_date = models.DateField(null=True)
+    notes = models.TextField(blank=True)
     current = models.BooleanField()
 
 
 class Parent(models.Model):
     parent = models.ForeignKey(Person, on_delete=models.DO_NOTHING)
-    biological = models.BooleanField()
-    notes = models.TextField()
+    biological = models.NullBooleanField(default=None) #Could change back on using booleanField if we only want yes or no 
+    notes = models.TextField(blank=True)
