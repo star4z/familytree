659349688faacabff728cdd75d1306f6a4b9e8a9
Cynthia(NAPS) <sally5610@gmail.com>
2020-02-04 17:32:10 -0500

    Added default values in Models

--- familytree/webapp/models.py
+++ familytree/webapp/models.py
@@ -2,13 +2,15 @@ from django.db import models
 
 prefix_choices = tuple()
 
+#Changes where First name and gender(?) has to be mandatory
+
 
 class Name(models.Model):
-    prefix = models.CharField(max_length=7, choices=prefix_choices, blank=True) 
-    first_name = models.TextField()
-    middle_name = models.TextField(blank=True)
-    last_name = models.TextField(blank=True)
-    suffix = models.CharField(max_length=6, blank=True)
+    prefix = models.CharField(max_length=7, choices=prefix_choices, blank=True, default='') 
+    first_name = models.TextField(default='')
+    middle_name = models.TextField(blank=True, default='')
+    last_name = models.TextField(blank=True, default='')
+    suffix = models.CharField(max_length=6, blank=True, default='')
 
 
 class LegalName(Name):
@@ -21,31 +23,31 @@ class AlternateName(Name):
 
 class Person(models.Model):
     name = models.OneToOneField(LegalName, on_delete=models.CASCADE)
-    preferred_name = models.TextField(blank=True)
-    birth_date = models.DateField(null=True)
-    death_date = models.DateField(null=True)
-    birth_location = models.TextField(blank=True) #TODO: change to location class
-    death_location = models.TextField(blank=True) #TODO: change to location class
-    living = models.BooleanField(default='TRUE')
-    gender = models.CharField(max_length=100) #Should gender be optional?
-    notes = models.TextField(blank=True)
-    occupations = models.TextField(blank=True)
-    partnerships = models.ManyToManyField('Partnership', blank=True, null=True) 
-    parents = models.ManyToManyField('Parent', blank=True, null=True)
+    preferred_name = models.TextField(blank=True, default='')
+    birth_date = models.DateField(null=True, blank=True)
+    death_date = models.DateField(null=True, blank=True)
+    birth_location = models.TextField(blank=True, default='') #TODO: change to location class
+    death_location = models.TextField(blank=True, default='') #TODO: change to location class
+    living = models.BooleanField(default=True)
+    gender = models.CharField(max_length=100, default='') #Should gender be optional?
+    notes = models.TextField(blank=True, default='')
+    occupations = models.TextField(blank=True, default='')
+    partnerships = models.ManyToManyField('Partnership', blank=True) 
+    parents = models.ManyToManyField('Parent', blank=True)
 
 
 class Partnership(models.Model):
     partner = models.ForeignKey(Person, on_delete=models.DO_NOTHING, related_name='+', null=True)
     children = models.ManyToManyField(Person, related_name='+')
-    married = models.BooleanField(default='FALSE')
-    marriage_date = models.DateField(null=True)
-    divorced = models.BooleanField(default='FALSE')
-    divorce_date = models.DateField(null=True)
-    notes = models.TextField(blank=True)
+    married = models.BooleanField(default=False)
+    marriage_date = models.DateField(null=True, blank =True)
+    divorced = models.BooleanField(default=False)
+    divorce_date = models.DateField(null=True, blank=True)
+    notes = models.TextField(blank=True, default='')
     current = models.BooleanField()
 
 
 class Parent(models.Model):
     parent = models.ForeignKey(Person, on_delete=models.DO_NOTHING)
     biological = models.NullBooleanField(default=None) #Could change back on using booleanField if we only want yes or no 
-    notes = models.TextField(blank=True)
+    notes = models.TextField(blank=True, default='')
