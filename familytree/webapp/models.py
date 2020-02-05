2040b73c1f628933849a211aff6bab53a2288723
unknown <tduong2049@gmail.com>
2020-02-05 16:57:26 -0500

    Created template for Location model

--- familytree/webapp/models.py
+++ familytree/webapp/models.py
@@ -18,14 +18,19 @@ class LegalName(Name):
 class AlternateName(Name):
     person = models.ForeignKey('Person', on_delete=models.DO_NOTHING)
 
+class Location(models.Model):
+    city = models.CharField(verbose_name='city/town/village', max_length=50)
+    state = models.CharField(verbose_name='state/province/region', 
+        max_length=50)
+    country = models.CharField(max_length=50)
 
 class Person(models.Model):
     name = models.OneToOneField(LegalName, on_delete=models.CASCADE)
     preferred_name = models.TextField()
     birth_date = models.DateField()
     death_date = models.DateField()
-    birth_location = models.TextField() #TODO: change to location class
-    death_location = models.TextField() #TODO: change to location class
+    birth_location = models.OneToOneField(Location, on_delete=models.CASCADE)
+    death_location = models.OneToOneField(Location, on_delete=models.CASCADE)
     living = models.BooleanField()
     gender = models.CharField(max_length=100)
     notes = models.TextField()
@@ -35,7 +40,8 @@ class Person(models.Model):
 
 
 class Partnership(models.Model):
-    partner = models.ForeignKey(Person, on_delete=models.DO_NOTHING, related_name='+', null=True)
+    partner = models.ForeignKey(Person, on_delete=models.DO_NOTHING, 
+        related_name='+', null=True)
     children = models.ManyToManyField(Person, related_name='+')
     married = models.BooleanField()
     marriage_date = models.DateField()
