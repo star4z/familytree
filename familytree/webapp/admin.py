080f7dc9711adf8228bb21061cc7e07e86c0e1dd
Benjamin Phillips <bdphillips013@gmail.com>
2020-02-10 00:37:50 -0500

    Changed extra line count for inlines

--- familytree/webapp/admin.py
+++ familytree/webapp/admin.py
@@ -4,18 +4,23 @@ from .models import AlternateName, LegalName, Parent, Partnership, Person
 
 class PersonInline(admin.TabularInline):
     model = Person
+    extra = 1
 
 class LegalNameInline(admin.TabularInline):
     model = LegalName
+    extra = 1
 
 class AlternateNameInline(admin.TabularInline):
     model = AlternateName
+    extra = 1
 
 class PartnershipInline(admin.TabularInline):
     model = Partnership
+    extra = 1
 
 class ParentInline(admin.TabularInline):
     model = Parent
+    extra = 1
 
 def legal_name(obj):
     return LegalName.objects.filter(person=obj)[0]
