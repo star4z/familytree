171510a4cf7935eb864bd6db72536bd364e5729c
Cynthia(NAPS) <sally5610@gmail.com>
2020-02-11 15:52:25 -0500

    Added an base view of web interface
    Added html and css files. Added urls.py to webapp.

--- familytree/webapp/views.py
+++ familytree/webapp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from webapp.models import Person, Partnership
+# create views here
 
-# Create your views here.
+def index(request):
+	# Generate counts of person
+	num_person = Person.objects.all().count()
+	num_partnerships = Partnership.objects.all().count()
+
+	context = {
+		'num_person': num_person,
+		'num_partnerships': num_partnerships,
+	}
+
+	return render(request, 'index.html', context=context)
