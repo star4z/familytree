ab61b85c8f44a12a72b105fc64c641abff5488c5
Benjamin Phillips <bdphillips013@gmail.com>
2020-02-13 11:45:26 -0500

    Partnerships are no longer required to have children.

--- familytree/webapp/models.py
+++ familytree/webapp/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from .submodels.location_model import Location
 
-
 prefix_choices = tuple()
 
+
 class Name(models.Model):
-    prefix = models.CharField(max_length=7, choices=prefix_choices, blank=True, default='') 
+    prefix = models.CharField(max_length=7, choices=prefix_choices, blank=True, default='')
     first_name = models.TextField(default='')
     middle_name = models.TextField(blank=True, default='')
     last_name = models.TextField(blank=True, default='')
@@ -14,23 +14,26 @@ class Name(models.Model):
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
-        
+
+
 class Person(models.Model):
     prefix = models.CharField(max_length=7, choices=prefix_choices, blank=True,
-        default='') 
+                              default='')
     first_name = models.TextField(default='')
     middle_name = models.TextField(blank=True, default='')
     last_name = models.TextField(blank=True, default='')
     suffix = models.CharField(max_length=6, blank=True, default='')
-    
+
     preferred_name = models.TextField(blank=True, default='')
     birth_date = models.DateField(null=True, blank=True)
     death_date = models.DateField(null=True, blank=True)
-    birth_location = models.ForeignKey(Location, related_name="birth_location", blank=True, on_delete=models.DO_NOTHING, null=True)
-    death_location = models.ForeignKey(Location, related_name="death_location", blank=True, on_delete=models.DO_NOTHING, null=True)
-    
+    birth_location = models.ForeignKey(Location, related_name="birth_location", blank=True, on_delete=models.DO_NOTHING,
+                                       null=True)
+    death_location = models.ForeignKey(Location, related_name="death_location", blank=True, on_delete=models.DO_NOTHING,
+                                       null=True)
+
     living = models.BooleanField(default=True)
-    gender = models.CharField(max_length=100, default='') #Should gender be optional?
+    gender = models.CharField(max_length=100, default='')  # Should gender be optional?
     notes = models.TextField(blank=True, default='')
     occupations = models.TextField(blank=True, default='')
     partnerships = models.ManyToManyField('Partnership', blank=True)
@@ -40,13 +43,13 @@ class Person(models.Model):
 
 
 class Partnership(models.Model):
-    children = models.ManyToManyField(Person, related_name='children')
+    children = models.ManyToManyField(Person, related_name='children', null=True)
     married = models.BooleanField(default=False)
-    marriage_date = models.DateField(null=True, blank =True)
+    marriage_date = models.DateField(null=True, blank=True)
     divorced = models.BooleanField(default=False)
     divorce_date = models.DateField(null=True, blank=True)
     notes = models.TextField(blank=True, default='')
-    current = models.BooleanField()
+    current = models.BooleanField(default=False)
 
     def __str__(self):
         return ', '.join(str(person) for person in Person.objects.filter(partnerships=self))
