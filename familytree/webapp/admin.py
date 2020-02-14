53ee3584e60a50a7df52b3bf8393fc6fdc4a8f4b
Benjamin Phillips <bdphillips013@gmail.com>
2020-02-13 20:31:32 -0500

    Changed TextField widget length to match that of CharField

--- familytree/webapp/admin.py
+++ familytree/webapp/admin.py
@@ -4,6 +4,7 @@ from django.forms import TextInput
 
 from .models import Location, Name, Partnership, Person
 
+
 class PersonInline(admin.TabularInline):
     model = Person
     extra = 1
@@ -13,24 +14,31 @@ class NameInline(admin.TabularInline):
     model = Name
     extra = 1
     formfield_overrides = {
-        models.TextField: {'widget': TextInput(attrs={'size':'20'})},
+        models.TextField: {'widget': TextInput(attrs={'size': '40'})},
     }
+
 
 @admin.register(Location)
 class LocationAdmin(admin.ModelAdmin):
     pass
 
+
 @admin.register(Person)
 class PersonAdmin(admin.ModelAdmin):
     inlines = [NameInline]
+    # fields = []
     list_display = ('first_name', 'last_name', 'birth_date', 'living', 'gender')
     formfield_overrides = {
-        models.TextField: {'widget': TextInput(attrs={'size':'20'})},
+        models.TextField: {'widget': TextInput(attrs={'size': '40'})},
     }
+
 
 def get_partners(obj):
     return ', '.join(str(person) for person in Person.objects.filter(partnerships=obj))
+
+
 get_partners.short_description = 'Partners'
+
 
 @admin.register(Partnership)
 class PartnershipAdmin(admin.ModelAdmin):
