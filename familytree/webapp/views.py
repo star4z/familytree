a3c144a68db526923e268466dcb64507164995e0
Cynthia(NAPS) <sally5610@gmail.com>
2020-02-24 23:22:45 -0500

    Add Person Form
    Currently unable to add partnerships. Need to format the forms

--- familytree/webapp/views.py
+++ familytree/webapp/views.py
@@ -1,18 +1,39 @@
 from django.shortcuts import render
 from django.views import generic
+from django.shortcuts import redirect
+from django.http import HttpResponseRedirect
 from webapp.models import Person, Partnership
-# create views here
+from webapp.forms import addPersonForm, NameForm
+
+def addPerson(request):
+    # if this is a POST request we need to process the form data
+    name_form = NameForm(request.POST)
+    person_form = addPersonForm(request.POST)
+    if request.method == 'POST':
+        # check whether it's valid:
+        if all((person_form.is_valid(), name_form.is_valid())):
+            profile = person_form.save(commit=False)
+            lname = name_form.save(commit=False)
+            lname.legal_name = profile
+            lname.save()
+            profile.save()
+            id = profile.id
+            # redirect to a new URL:
+            return redirect('person_detail', pk=id)
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        name_form = NameForm()
+        person_form = addPersonForm()
+    return render(request, 'webapp/createPerson.html', {'name_form': name_form, 'person_form': person_form})
 
 def index(request):
 	# Generate counts of person
 	num_person = Person.objects.all().count()
 	num_partnerships = Partnership.objects.all().count()
-
 	context = {
 		'num_person': num_person,
 		'num_partnerships': num_partnerships,
 	}
-
 	return render(request, 'index.html', context=context)
 
 class PersonListView(generic.ListView):
@@ -25,11 +46,3 @@ class PartnershipListView(generic.ListView):
 
 class PersonDetailView(generic.DetailView):
 	model = Person
-"""
-	 def get_context_data(self, **kwargs):
-        # Call the base implementation first to get the context
-        context = super(BookListView, self).get_context_data(**kwargs)
-        # Create any data and add it to the context
-        context['some_data'] = 'This is just some data'
-        return context
-"""
