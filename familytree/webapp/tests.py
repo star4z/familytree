4007afad9758050a5d72af23da618485b3899689
Benjamin Phillips <bdphillips013@gmail.com>
2020-03-02 20:17:37 -0500

    Updated tests

--- familytree/webapp/tests.py
+++ familytree/webapp/tests.py
@@ -4,55 +4,54 @@ from webapp import models
 
 class PersonTestCase(TestCase):
     def setUp(self):
-        tree = models.Tree.objects.create(title='test tree')
-        partnerships = [self.create_partnership(tree) for _ in range(5)]
-        # persons = [
-        self.abe = self.create_person(tree, 'Abe', 'M', partnerships[0])
-        self.beth = self.create_person(tree, 'Beth', 'F', partnerships[0])
-        self.dave = self.create_person(tree, 'Dave', 'M', partnerships[1])
-        self.jeanine = self.create_person(tree, 'Jeanine', 'F', partnerships[1])
-        self.philip = self.create_person(tree, 'Philip', 'M', partnerships[2])
-        self.megumi = self.create_person(tree, 'Megumi', 'F', partnerships[2])
-        self.akito = self.create_person(tree, 'Akito', 'M', partnerships[3])
-        self.nala = self.create_person(tree, 'Nala', 'F', partnerships[3])
-        self.colin = self.create_person(tree, 'Colin', 'M')
-        self.akira = self.create_person(tree, 'Akira', 'F')
-        self.elizabeth = self.create_person(tree, 'Elizabeth', 'F', partnerships[4])
-        self.kassandra = self.create_person(tree, 'Kassandra', 'F', partnerships[4])
-        self.john = self.create_person(tree, 'John', 'M')
-        self.violet = self.create_person(tree, 'Violet', 'F')
-        # ]
+        self.tree = models.Tree.objects.create(title='test tree')
 
-        partnerships[0].children.add(self.dave)  # Dave is Abe and Beth's child
-        partnerships[1].children.add(self.philip)  # Philip is Dave and Jeanine's child
-        partnerships[2].children.add(self.akito)  # Akito is Philip and Megumi's child
-        partnerships[3].children.add(self.colin)  # Colin is Akito and Nala's child
-        partnerships[3].children.add(self.akira)  # Akira is Akito and Nala's child
-        partnerships[2].children.add(self.elizabeth)  # Elizabeth is Philip and Megumi's child
-        partnerships[4].children.add(self.john)  # John is Elizabeth and Kassandra's child
-        partnerships[4].children.add(self.violet)  # Violet is Elizabeth and Kassandra's child
+        self.abe = self.create_person('Abe', 'M', [1])
+        self.beth = self.create_person('Beth', 'F', [1])
+        self.dave = self.create_person('Dave', 'M', [2])
+        self.jeanine = self.create_person('Jeanine', 'F', [2])
+        self.philip = self.create_person('Philip', 'M', [3])
+        self.megumi = self.create_person('Megumi', 'F', [3])
+        self.akito = self.create_person('Akito', 'M', [4])
+        self.nala = self.create_person('Nala', 'F', [4])
+        self.colin = self.create_person('Colin', 'M')
+        self.akira = self.create_person('Akira', 'F')
+        self.elizabeth = self.create_person('Elizabeth', 'F', [5])
+        self.kassandra = self.create_person('Kassandra', 'F', [5])
+        self.john = self.create_person('John', 'M')
+        self.violet = self.create_person('Violet', 'F')
+        self.pablo = self.create_person('Pablo', 'M', )
 
-        self.gen2 = [partnerships[0]]
-        self.gen1 = [partnerships[1]]
+        self.get_partnership(1).children.add(self.dave)  # Dave is Abe and Beth's child
+        self.get_partnership(2).children.add(self.philip)  # Philip is Dave and Jeanine's child
+        self.get_partnership(3).children.add(self.akito)  # Akito is Philip and Megumi's child
+        self.get_partnership(4).children.add(self.colin)  # Colin is Akito and Nala's child
+        self.get_partnership(4).children.add(self.akira)  # Akira is Akito and Nala's child
+        self.get_partnership(3).children.add(self.elizabeth)  # Elizabeth is Philip and Megumi's child
+        self.get_partnership(5).children.add(self.john)  # John is Elizabeth and Kassandra's child
+        self.get_partnership(5).children.add(self.violet)  # Violet is Elizabeth and Kassandra's child
+
+        self.gen2 = [self.get_partnership(1)]
+        self.gen1 = [self.get_partnership(2)]
         self.gen0 = [self.philip]
         self.genN1 = [self.akito, self.elizabeth]
         self.genN2 = [self.colin, self.akira, self.john, self.violet]
 
-    @staticmethod
-    def create_person(tree, first_name, gender, partnership=None):
+    def create_person(self, first_name, gender, partnership_ids=None):
         genders = {'M': 'Male', 'F': 'Female'}
         legal_name = models.LegalName.objects.create(first_name=first_name)
         legal_name.save()
-        p = models.Person.objects.create(tree=tree, legal_name=legal_name, gender=genders[gender])
+        p = models.Person.objects.create(tree=self.tree, legal_name=legal_name, gender=genders[gender])
         p.save()
-        if partnership:
-            person_partnership = models.PersonPartnership.objects.create(person=p, partnership=partnership)
-            person_partnership.save()
+        if partnership_ids:
+            for pid in partnership_ids:
+                partnership = self.get_partnership(pid)
+                person_partnership = models.PersonPartnership.objects.create(person=p, partnership=partnership)
+                person_partnership.save()
         return p
 
-    @staticmethod
-    def create_partnership(tree):
-        partnership = models.Partnership.objects.create(tree=tree)
+    def get_partnership(self, pid):
+        partnership, created = models.Partnership.objects.get_or_create(id=pid, defaults={'tree': self.tree})
         partnership.save()
         return partnership
 
