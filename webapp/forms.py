df62f33222920d51847690011fe5c1af41457577
unknown <tduong2049@gmail.com>
2020-03-09 19:42:06 -0400

    Updated Person Form's field ordering

--- webapp/forms.py
+++ webapp/forms.py
@@ -3,7 +3,6 @@ from django.forms import ModelForm
 from django.forms import BaseModelFormSet
 from webapp.models import Person, LegalName, Location, Partnership, AlternateName
 
-
 class AddNameForm(ModelForm):
     class Meta:
         model = LegalName
@@ -17,16 +16,35 @@ class AddNameForm(ModelForm):
         }
 
 class AddPersonForm(ModelForm):
+    blank_choice = [('','----------------')]
+    birth_city = forms.CharField(label='City/Town/Village', 
+        max_length=50, required=False)
+    birth_state = forms.CharField(label='State/Province/Region',
+        max_length=50, required=False)
+    birth_country = forms.ChoiceField(label='Country', 
+        choices=blank_choice + Location.Country.choices, required=False)
+
+    death_city = forms.CharField(label='City/Town/Village',
+        max_length=50, required=False)
+    death_state = forms.CharField(label='State/Province/Region', 
+        max_length=50, required=False)
+    death_country = forms.ChoiceField(label='Country', 
+        choices=blank_choice + Location.Country.choices, required=False)
+
     class Meta:
         model = Person
-        exclude = ['legal_name', 'tree', 'birth_location', 'death_location']
+        fields = ['preferred_name', 'gender', 'birth_date', 'death_date', 
+            'living', 'notes']
         widgets = {
+            'preferred_name': forms.TextInput(attrs={'size': '40'}),
             'birth_date': forms.DateInput(attrs={'type': 'date'}),
             'death_date': forms.DateInput(attrs={'type': 'date'}),
-            'preferred_name': forms.TextInput(attrs={'size': '40'}),
             'notes': forms.Textarea(attrs={'rows': 10, 'cols': '50'})
         }
 
+    field_order = ['preferred_name', 'gender', 'birth_date', 'birth_city', 
+        'birth_state','birth_country', 'living', 'death_date', 'death_city', 
+        'death_state', 'death_country', 'notes']
 
 class AlternateNameForm(ModelForm):
     first_name = forms.CharField(required=False)
@@ -41,7 +59,6 @@ class AlternateNameForm(ModelForm):
             'suffix': forms.TextInput(attrs={'size': '3'})
         }
 
-
 # Defines a Location form based on the Location model.
 # Includes every attribute.
 # Textfield sizes for 'city' and 'state' attributes set to 50 characters.
@@ -53,7 +70,6 @@ class AddLocationForm(ModelForm):
             'city': forms.TextInput(attrs={'size': '50'}),
             'state': forms.TextInput(attrs={'size': '50'})
         }
-
 
 class AddPartnershipForm(ModelForm):
     class Meta:
