b6f9d1cbe4bf561eaef080d9e97fc24430999662
Benjamin Phillips <bdphillips013@gmail.com>
2020-03-10 20:10:42 -0400

    Update script.sql

--- script.sql
+++ script.sql
@@ -1,6 +1,5 @@
 DROP DATABASE IF EXISTS familytreedb;
 CREATE DATABASE familytreedb;
 CREATE USER 'familytreedefault'@'localhost' IDENTIFIED BY 'familytreedefaultpwd';
-GRANT ALL PRIVILEGES ON familytreedb.* TO 'familytreedefault'@'localhost' IDENTIFIED BY 'familytreedefaultpwd' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'familytreedefault'@'localhost' IDENTIFIED BY 'familytreedefaultpwd' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
-ALTER USER 'familytreedefault' CREATEDB;
