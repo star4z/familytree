f336e90ec14fa6f352e9ce7a0fdb1004e76c5d2a
Benjamin Phillips <bdphillips013@gmail.com>
2020-03-19 15:44:48 -0400

    Converted graph script to object-oriented style

--- webapp/static/webapp/js/graph_person.js
+++ webapp/static/webapp/js/graph_person.js
@@ -1,71 +1,127 @@
 person_id = JSON.parse(document.getElementById('person_id').textContent);
 persons = JSON.parse(document.getElementById('persons').textContent);
 
-console.log(person_id);
-console.log(persons);
-
 person = persons[person_id];
 
 person_box = {height: 30, width: 100};
 box_padding = {vert: 10, horiz: 10};
 
-//row width = length of the box for each person in the row, plus the padding between them.
-row0_width = person.parents.length * person_box.width + 2 * (person.parents.length - 1) * box_padding.horiz;
-row1_width = person_box.width + person.partnerships.length * (person_box.width + 2 * box_padding.horiz);
-num_children = 0;
-for (partnership of person.partnerships) {
-    num_children += partnership.children.length;
+const sum = (a, b) => a + b;
+
+/**
+ * Contains an array of Items to be graphed at roughly the same y-value.
+ */
+class Row {
+    constructor(contents) {
+        this.contents = contents;
+    }
+
+    width = function () {
+        return this.contents.map(item => item.full_width()).reduce(sum);
+    };
+
+    height = function() {
+        return Math.max(this.contents.map(item => item.full_height()));
+    };
 }
-row2_width = num_children * person_box.width + (num_children - 1) * box_padding.horiz;
 
-console.log(row0_width);
-console.log(row1_width);
-console.log(row2_width);
+/**
+ * Contains values and type info for graphing
+ */
+class Item {
+    constructor() {
+        //Define defaults. These should not be overwritten. These values can be used to inform the actual values.
+        this.defaults = {};
+        this.defaults.padding = {top: 10, right: 10, bottom: 10, left: 10};
+        this.defaults.width = 100;
+        this.defaults.height = 30;
 
-row0_start = 0;
-row1_start = row0_start + (row0_width - person_box.width) / 2;
-row2_start = row1_start + (row1_width - row2_width) / 2;
-min_start = Math.min(row0_start, row1_start, row2_start);
-row0_start -= min_start;
-row1_start -= min_start;
-row2_start -= min_start;
+        //Set these values in instances to manipulate.
+        this.padding = {};
+        Object.assign(this.padding, this.defaults.padding);
+        this.width = this.defaults.width;
+        this.height = this.defaults.height;
+    }
 
-console.log(row0_start);
-console.log(row1_start);
-console.log(row2_start);
+    full_width() {
+        return this.padding.left + this.width + this.padding.right;
+    };
 
-row0_y = 0;
-row1_y = row0_y + person_box.height + box_padding.vert;
-row2_y = row1_y + person_box.height + 2 * box_padding.vert;
+    full_height() {
+        return this.padding.top + this.height + this.padding.bottom;
+    };
+
+    graph(x, y, ctx) {
+    }
+}
+
+class Partnership extends Item {
+    constructor(partners, children) {
+        super();
+        this.partners = partners;
+        this.children = children;
+    }
+
+    full_width() {
+        return 2 * super.full_width();
+    }
+
+    graph(x, y, ctx) {
+        const offset1 = {x: x + this.padding.left, y: y + this.padding.top};
+        ctx.fillRect(offset1.x, offset1.y, this.defaults.width, this.height);
+        const offset2 = {
+            x: x + this.padding.left + this.defaults.width + this.padding.right + this.padding.left,
+            y: y + this.padding.top
+        };
+        ctx.fillRect(offset2.x, offset2.y, this.width, this.height);
+        ctx.moveTo(x + this.padding.left + this.width, y + this.padding.top + this.height / 2);
+        ctx.lineTo(x + 2 * this.padding.left + this.padding.right + this.width, y + this.padding.top + this.height / 2);
+        ctx.stroke();
+    }
+}
+
+class Person extends Item {
+    constructor(id) {
+        super();
+        this.id = id;
+    }
+
+    graph(x, y, ctx) {
+        ctx.fillRect(x + this.padding.left, y + this.padding.top, this.width, this.height);
+    }
+}
+
+let person_partners = [];
+let children = [];
+for (let partnership of person.partnerships) {
+    person_partners = person_partners.concat(partnership.partners);
+    children = children.concat(partnership.children);
+}
+person_partners.push(person.id);
+
+rows = [
+    new Row([
+        new Partnership(person.parents, [person.id]),
+    ]),
+    new Row([
+        new Partnership(person_partners, children),
+    ]),
+    new Row(
+        children.map(child => new Person(child))
+    ),
+];
 
 canvas = document.getElementById("person-graph");
 ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-// ctx.fillStyle = "#FF0000";
-x = row0_start;
-for (parent of person.parents) {
-    ctx.fillRect(x, row0_y, person_box.width, person_box.height);
-    x += person_box.width + 2 * box_padding.horiz;
-}
-x = row1_start;
-ctx.fillRect(x, row1_y, person_box.width, person_box.height);
-x += person_box.width + 2 * box_padding.horiz;
-for (partnership of person.partnerships) {
-    for (partner of partnership.partners) {
-        ctx.fillRect(x, row1_y, person_box.width, person_box.height);
-        x += person_box.width + 2 * box_padding.horiz;
+let y = 0;
+for (let row of rows) {
+    let x = 0;
+    for (item of row.contents) {
+        item.graph(x, y, ctx);
+        x += item.full_width();
     }
+    y += row.height();
 }
-x = row2_start;
-for (partnership of person.partnerships) {
-    for (child of partnership.children) {
-        ctx.fillRect(x, row2_y, person_box.width, person_box.height);
-        x += person_box.width + 2 * box_padding.horiz;
-    }
-}
-// ctx.moveTo(0, 0);
-// ctx.lineTo(500, 500);
-// ctx.stroke();
-// ctx.fillRect(0, 0, 500, 500);
