a3674f99bf14dbca634cc0a7d171c4cf7b2aed3f
Cynthia(NAPS) <sally5610@gmail.com>
2020-03-28 19:47:39 -0400

    Add user validation to add_person

--- webapp/views.py
+++ webapp/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views import generic
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
 from webapp.models import Person, Partnership, Location, LegalName, AlternateName, Tree
 from webapp.forms import AddPersonForm, AddNameForm, AddTreeForm, AddPartnershipForm, AlternateNameFormSet
 from django.views.generic.edit import CreateView
@@ -32,87 +32,92 @@ def add_tree(request):
 
 @login_required
 def add_person(request, tree_pk):
-    if request.method == 'POST':
-        # if this is a POST request we need to process the form data
-        name_form = AddNameForm(request.POST)
-        person_form = AddPersonForm(request.POST)
+    current_tree = Tree.objects.get(pk=tree_pk)
 
-        current_tree = Tree.objects.get(pk=tree_pk)
+    # User validation to prevent other users from adding to trees that aren't theirs
+    if current_tree.creator == request.user:
+        if request.method == 'POST':
+            # if this is a POST request we need to process the form data
+            name_form = AddNameForm(request.POST)
+            person_form = AddPersonForm(request.POST)
 
-        # Tuple that contains validation status of each filled form
-        form_validations = (
-            person_form.is_valid(),
-            name_form.is_valid(),
-        )
+            # Tuple that contains validation status of each filled form
+            form_validations = (
+                person_form.is_valid(),
+                name_form.is_valid(),
+            )
 
-        # check whether it's valid:
-        if all(form_validations):
-            # Create a Legal Name instance from name form's data
+            # check whether it's valid:
+            if all(form_validations):
+                # Create a Legal Name instance from name form's data
 
-            created_legal_name = name_form.save(commit=False)
-            created_legal_name.tree = current_tree
-            created_legal_name.save()
+                created_legal_name = name_form.save(commit=False)
+                created_legal_name.tree = current_tree
+                created_legal_name.save()
 
-            # Create a Person instance from person form's data
-            # Person instance's Legal Name attribute will be a foreign key
-            created_person = person_form.save(commit=False)
-            created_person.tree = current_tree
-            created_person.legal_name = created_legal_name
-            created_person.save()
+                # Create a Person instance from person form's data
+                # Person instance's Legal Name attribute will be a foreign key
+                created_person = person_form.save(commit=False)
+                created_person.tree = current_tree
+                created_person.legal_name = created_legal_name
+                created_person.save()
 
-            # Create Alternate Name for person
-            alt_name_formset = AlternateNameFormSet(request.POST, instance=created_person)
-            if alt_name_formset.is_valid():
-                alt_names = alt_name_formset.save(commit=False)
-                for alt_name in alt_names:
-                    alt_name.tree = current_tree
-                    alt_name.save()
+                # Create Alternate Name for person
+                alt_name_formset = AlternateNameFormSet(request.POST, instance=created_person)
+                if alt_name_formset.is_valid():
+                    alt_names = alt_name_formset.save(commit=False)
+                    for alt_name in alt_names:
+                        alt_name.tree = current_tree
+                        alt_name.save()
 
-            # Check each location form's data and query for existing Location
-            # instances.
-            # If location exists, stores it in the corresponding location
-            # variable and sets location_created boolean to false
-            # If it doesn't exist, create a new instance from form's data and
-            # set location_created boolean to true
-            birth_location, birth_loc_was_created = Location.objects.get_or_create(
-                city=person_form.cleaned_data['birth_city'],
-                state=person_form.cleaned_data['birth_state'],
-                country=person_form.cleaned_data['birth_country'])
+                # Check each location form's data and query for existing Location
+                # instances.
+                # If location exists, stores it in the corresponding location
+                # variable and sets location_created boolean to false
+                # If it doesn't exist, create a new instance from form's data and
+                # set location_created boolean to true
+                birth_location, birth_loc_was_created = Location.objects.get_or_create(
+                    city=person_form.cleaned_data['birth_city'],
+                    state=person_form.cleaned_data['birth_state'],
+                    country=person_form.cleaned_data['birth_country'])
 
-            death_location, death_loc_was_created = Location.objects.get_or_create(
-                city=person_form.cleaned_data['death_city'],
-                state=person_form.cleaned_data['death_state'],
-                country=person_form.cleaned_data['death_country'])
+                death_location, death_loc_was_created = Location.objects.get_or_create(
+                    city=person_form.cleaned_data['death_city'],
+                    state=person_form.cleaned_data['death_state'],
+                    country=person_form.cleaned_data['death_country'])
 
-            # if new location instances were created, save them in the DB
-            if birth_loc_was_created:
-                birth_location.save()
+                # if new location instances were created, save them in the DB
+                if birth_loc_was_created:
+                    birth_location.save()
 
-            if death_loc_was_created:
-                death_location.save()
+                if death_loc_was_created:
+                    death_location.save()
 
-            # Assign the location instances as keys in Person instance
-            created_person.birth_location = birth_location
-            created_person.death_location = death_location
+                # Assign the location instances as keys in Person instance
+                created_person.birth_location = birth_location
+                created_person.death_location = death_location
 
-            created_person.save()
+                created_person.save()
 
-            # redirect to page containing new Person instance's details
-            return redirect('person_detail', pk=created_person.id)
+                # redirect to page containing new Person instance's details
+                return redirect('person_detail', pk=created_person.id)
 
-    # if a GET (or any other method) we'll create a blank form
+        # if a GET (or any other method) we'll create a blank form
+        else:
+            name_form = AddNameForm()
+            person_form = AddPersonForm()
+            alt_name_formset = AlternateNameFormSet()
+
+        context = {
+            'name_form': name_form,
+            'person_form': person_form,
+            'alt_name_formset': alt_name_formset,
+        }
+
+        return render(request, 'webapp/add_person.html', context)
+
     else:
-        name_form = AddNameForm()
-        person_form = AddPersonForm()
-        alt_name_formset = AlternateNameFormSet()
-
-    context = {
-        'name_form': name_form,
-        'person_form': person_form,
-        'alt_name_formset': alt_name_formset,
-    }
-
-    return render(request, 'webapp/add_person.html', context)
+        raise Http404
 
 
 @login_required
