19b718d0a39d7e98bcae9a3966bab519af4de6ee
Benjamin Phillips <bdphillips013@gmail.com>
2020-03-29 13:29:34 -0400

    Uncommented code to graph parents

--- webapp/static/webapp/js/graph_person.js
+++ webapp/static/webapp/js/graph_person.js
@@ -19,51 +19,51 @@ data.nodes.push({
 
 added_people = [person_id];
 
-// for (let m_person of Object.values(persons)) {
-//     if (!(added_people.includes(m_person.id))) {
-//         for (let m_partnership of m_person.partnerships) {
-//             if (m_partnership.children.includes(person.id)) {
-//                 added_people.push(m_person.id);
-//                 console.log(data.nodes);
-//                 data.nodes.push({
-//                         id: m_person.id.toString(),
-//                         x: -50,
-//                         y: -50
-//                     },
-//                     {
-//                         id: 'p' + person.id.toString(),
-//                         x: 0,
-//                         y: -50
-//                     }
-//                 );
-//                 console.log(data.nodes);
-//                 data.edges.push(
-//                     {
-//                         source: person.id.toString(),
-//                         target: 'p' + person.id.toString(),
-//                     },
-//                     {
-//                         source: m_person.id.toString(),
-//                         target: 'p' + person.id.toString()
-//                     }
-//                 );
-//                 if (m_partnership.partners.length !== 0) {
-//                     let m_partner = m_partnership.partners[0];
-//                     data.nodes.push({
-//                         id: m_partner.toString(),
-//                         x: 50,
-//                         y: -50
-//                     });
-//                     data.edges.push({
-//                        source: m_partner.toString(),
-//                        target: 'p' + person.id.toString()
-//                     });
-//                     added_people.push(m_partner);
-//                 }
-//             }
-//         }
-//     }
-// }
+for (let m_person of Object.values(persons)) {
+    if (!(added_people.includes(m_person.id))) {
+        for (let m_partnership of m_person.partnerships) {
+            if (m_partnership.children.includes(person.id)) {
+                added_people.push(m_person.id);
+                console.log(data.nodes);
+                data.nodes.push({
+                        id: m_person.id.toString(),
+                        x: -50,
+                        y: -50
+                    },
+                    {
+                        id: 'p' + person.id.toString(),
+                        x: 0,
+                        y: -50
+                    }
+                );
+                console.log(data.nodes);
+                data.edges.push(
+                    {
+                        source: person.id.toString(),
+                        target: 'p' + person.id.toString(),
+                    },
+                    {
+                        source: m_person.id.toString(),
+                        target: 'p' + person.id.toString()
+                    }
+                );
+                if (m_partnership.partners.length !== 0) {
+                    let m_partner = m_partnership.partners[0];
+                    data.nodes.push({
+                        id: m_partner.toString(),
+                        x: 50,
+                        y: -50
+                    });
+                    data.edges.push({
+                       source: m_partner.toString(),
+                       target: 'p' + person.id.toString()
+                    });
+                    added_people.push(m_partner);
+                }
+            }
+        }
+    }
+}
 
 let minX = 0;
 let minY = 0;
