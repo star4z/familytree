912f02078571628a2a142059dac41751c7ad3207
Benjamin Phillips <bdphillips013@gmail.com>
2020-03-28 23:17:18 -0400

    Added g6 graphing for simple case

--- webapp/static/webapp/js/graph_person.js
+++ webapp/static/webapp/js/graph_person.js
@@ -1,127 +1,112 @@
-person_id = JSON.parse(document.getElementById('person_id').textContent);
-persons = JSON.parse(document.getElementById('persons').textContent);
+let person_id = JSON.parse(document.getElementById('person_id').textContent);
+let persons = JSON.parse(document.getElementById('persons').textContent);
 
-person = persons[person_id];
+console.log(persons);
 
-person_box = {height: 30, width: 100};
-box_padding = {vert: 10, horiz: 10};
+let person = persons[person_id];
+//
+// function getOldest(pid) {
+//     let older = [];
+//     for (let parent of persons[pid].parents) {
+//         //TODO: check for circular trees
+//         if (parent.id in persons) {
+//             older.push(parent.id);
+//         }
+//     }
+//     if (older.length !== 0) {
+//         let oldest = [];
+//         for (let person of older) {
+//             let elder = getOldest(person);
+//             oldest = oldest.concat(elder);
+//         }
+//         return oldest;
+//     } else {
+//         return [pid];
+//     }
+// }
+//
+// let oldest = getOldest(person_id);
+//
+// console.log(oldest);
 
-const sum = (a, b) => a + b;
+const data = {
+    nodes: [],
+    edges: []
+};
 
-/**
- * Contains an array of Items to be graphed at roughly the same y-value.
- */
-class Row {
-    constructor(contents) {
-        this.contents = contents;
-    }
 
-    width = function () {
-        return this.contents.map(item => item.full_width()).reduce(sum);
-    };
+data.nodes.push({
+    id: person.id.toString(),
+    x: 0,
+    y: 0
+});
 
-    height = function() {
-        return Math.max(this.contents.map(item => item.full_height()));
-    };
-}
+added_people = [person_id];
 
-/**
- * Contains values and type info for graphing
- */
-class Item {
-    constructor() {
-        //Define defaults. These should not be overwritten. These values can be used to inform the actual values.
-        this.defaults = {};
-        this.defaults.padding = {top: 10, right: 10, bottom: 10, left: 10};
-        this.defaults.width = 100;
-        this.defaults.height = 30;
 
-        //Set these values in instances to manipulate.
-        this.padding = {};
-        Object.assign(this.padding, this.defaults.padding);
-        this.width = this.defaults.width;
-        this.height = this.defaults.height;
-    }
-
-    full_width() {
-        return this.padding.left + this.width + this.padding.right;
-    };
-
-    full_height() {
-        return this.padding.top + this.height + this.padding.bottom;
-    };
-
-    graph(x, y, ctx) {
-    }
-}
-
-class Partnership extends Item {
-    constructor(partners, children) {
-        super();
-        this.partners = partners;
-        this.children = children;
-    }
-
-    full_width() {
-        return 2 * super.full_width();
-    }
-
-    graph(x, y, ctx) {
-        const offset1 = {x: x + this.padding.left, y: y + this.padding.top};
-        ctx.fillRect(offset1.x, offset1.y, this.defaults.width, this.height);
-        const offset2 = {
-            x: x + this.padding.left + this.defaults.width + this.padding.right + this.padding.left,
-            y: y + this.padding.top
-        };
-        ctx.fillRect(offset2.x, offset2.y, this.width, this.height);
-        ctx.moveTo(x + this.padding.left + this.width, y + this.padding.top + this.height / 2);
-        ctx.lineTo(x + 2 * this.padding.left + this.padding.right + this.width, y + this.padding.top + this.height / 2);
-        ctx.stroke();
+for (let m_person of Object.values(persons)) {
+    for (let m_partnership of m_person.partnerships) {
+        if (person.id in m_partnership.children) {
+            data.nodes.push({
+                    id: m_person.id.toString(),
+                    x: -50,
+                    y: -50
+                },
+                {
+                    id: 'p' + person.id.toString(),
+                    x: 50,
+                    y: -75
+                }
+            );
+            data.edges.push(
+                {
+                    source: person.id.toString(),
+                    target: 'p' + person.id.toString(),
+                },
+                {
+                    source: m_person.id.toString(),
+                    target: 'p' + person.id.toString()
+                });
+        }
     }
 }
+//
+// for (let i of oldest) {
+//     let x = 0;
+//     data.nodes.push({
+//         id: persons[i].name,
+//         x: x,
+//         y: 0,
+//     });
+//     data.edges.push({
+//         source: persons[i].name,
+//     });
+//     x += 100;
+// }
 
-class Person extends Item {
-    constructor(id) {
-        super();
-        this.id = id;
+let minX = 0;
+let minY = 0;
+//Find minX and minY
+for (let node of data.nodes) {
+    if (node.x < minX) {
+        minX = node.x;
     }
-
-    graph(x, y, ctx) {
-        ctx.fillRect(x + this.padding.left, y + this.padding.top, this.width, this.height);
+    if (node.y < minY) {
+        minY = node.Y;
     }
 }
 
-let person_partners = [];
-let children = [];
-for (let partnership of person.partnerships) {
-    person_partners = person_partners.concat(partnership.partners);
-    children = children.concat(partnership.children);
+//Shift all nodes to make all values positive
+for (let node of data.nodes) {
+    node.x += 50 - minX;
+    node.y += 50 - minY;
 }
-person_partners.push(person.id);
 
-rows = [
-    new Row([
-        new Partnership(person.parents, [person.id]),
-    ]),
-    new Row([
-        new Partnership(person_partners, children),
-    ]),
-    new Row(
-        children.map(child => new Person(child))
-    ),
-];
+const graph = new G6.Graph({
+    container: "family-graph",
+    width: 800,
+    height: 500,
+});
 
-canvas = document.getElementById("person-graph");
-ctx = canvas.getContext("2d");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-let y = 0;
-for (let row of rows) {
-    let x = 0;
-    for (item of row.contents) {
-        item.graph(x, y, ctx);
-        x += item.full_width();
-    }
-    y += row.height();
-}
+graph.data(data);
+graph.render();
