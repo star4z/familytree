d5dd9cc6fd01fb5d81b4ffee0a3671af2cf883ab
Benjamin Phillips <bdphillips013@gmail.com>
2020-03-31 19:54:51 -0400

    Clicking on a node now leads to the graph for that node

--- webapp/static/webapp/js/graph_person.js
+++ webapp/static/webapp/js/graph_person.js
@@ -2,10 +2,31 @@ data = JSON.parse(document.getElementById('data').textContent);
 
 console.log(data);
 
+
+G6.registerBehavior('activate-node', {
+    getEvents() {
+        return {
+            'node:click': 'onNodeClick',
+        };
+    },
+    onNodeClick(e) {
+        const item = e.item;
+        console.log(item);
+        let id_pieces = item._cfg.id.split('_');
+        if (id_pieces.length === 2 && id_pieces[0] === 'Person') {
+            let link = id_pieces[id_pieces.length - 1];
+            window.location = '/webapp/person/' + link + '/graph/';
+        }
+    }
+});
+
 const graph = new G6.Graph({
     container: "family-graph",
     width: 500,
     height: 500,
+    modes: {
+        default: ['activate-node'],
+    },
 });
 
 graph.data(data);
