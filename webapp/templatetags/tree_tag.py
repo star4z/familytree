45bedec5537a96dfd9760d80f15feeea250551b9
Cynthia(NAPS) <sally5610@gmail.com>
2020-04-28 15:14:21 -0400

    fixed paginator

--- webapp/templatetags/tree_tag.py
+++ webapp/templatetags/tree_tag.py
@@ -7,15 +7,9 @@ register = template.Library()
 @register.inclusion_tag('webapp/sidebar_tree_list.html', takes_context=True)
 def render_sidebar_trees(context):
     request = context['request']
-
     sidebar_tree_list = Tree.objects.filter(creator=request.user).order_by("id")
+
     paginator = Paginator(sidebar_tree_list, 5)
+    trees = paginator.page(1)
 
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
-
-    contexts = {
-        'sidebar_tree_list': sidebar_tree_list,
-        'page_obj': page_obj
-    }
-    return contexts
+    return {'trees': trees}
