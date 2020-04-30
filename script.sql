015d6c2517025c712590e9c6c33c87707c2262d6
Benjamin Phillips <bdphillips013@gmail.com>
2020-04-30 19:31:01 -0400

    Cleaned up action to perform better

--- script.sql
+++ script.sql
@@ -1,5 +1,6 @@
 DROP DATABASE IF EXISTS familytreedb;
 CREATE DATABASE familytreedb;
+DROP USER IF EXISTS 'familytreedefault'@'localhost';
 CREATE USER 'familytreedefault'@'localhost' IDENTIFIED BY 'familytreedefaultpwd';
 GRANT ALL PRIVILEGES ON *.* TO 'familytreedefault'@'localhost' IDENTIFIED BY 'familytreedefaultpwd' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
