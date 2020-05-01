f5bf41e5912841a6bc3fbd89f05081329ec4caa5
Benjamin Phillips <bdphillips013@gmail.com>
2020-05-01 09:34:27 -0400

    Added allauth to installed apps

--- familytree/settings.py
+++ familytree/settings.py
@@ -39,6 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
 ]
 
 MIDDLEWARE = [
