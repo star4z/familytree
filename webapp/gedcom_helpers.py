581ba5d16d0fee8983dc96769a7070724adfb816
Benjamin Phillips <bdphillips013@gmail.com>
2020-08-23 00:15:15 -0400

    Adds more gedcom helper methods and fixes issue with filter_child_elements

--- webapp/gedcom_helpers.py
+++ webapp/gedcom_helpers.py
@@ -18,9 +18,9 @@ Element.get_next_child_element = get_next_child_element
 
 def filter_child_elements(self: Element, tag: str = None, pointer: str = None, value: str = None):
     return [child for child in self.get_child_elements()
-            if (pointer is not None and child.get_pointer() == pointer)
-            and (tag is not None and child.get_tag() == tag)
-            and (value is not None and child.get_value() == value)]
+            if (tag is None or child.get_tag() == tag)
+            and (pointer is None or child.get_pointer() == pointer)
+            and (value is None or child.get_value() == value)]
 
 
 Element.filter_child_elements = filter_child_elements
@@ -66,3 +66,51 @@ def get_names(self: IndividualElement):
     if names:
         return (HumanName(name.get_value()).as_dict() for name in names)
     return get_name_dict_from_name_tags(self),
+
+
+def get_value(self: Element, tag: str):
+    """
+    Gets the value of a child with the given tag
+    :param self: Element whose children will be searched
+    :param tag: tag to look for
+    :return: the value of child with the given tag, else empty string
+    """
+    for child in self.get_child_elements():
+        if child.get_tag() == tag:
+            return child.get_value()
+
+    return ""
+
+
+def create_individual(ptr: str,
+                      name: str = None,
+                      sex: str = None,
+                      birth_place: str = None,
+                      birth_date: str = None,
+                      death_place: str = None,
+                      death_date: str = None,
+                      family_spouse_ptr: str = None,
+                      family_child_ptr: str = None):
+    individual_element = IndividualElement(0, ptr, tags.GEDCOM_TAG_INDIVIDUAL, '')
+    if name is not None:
+        individual_element.add_child_element(Element(1, '', tags.GEDCOM_TAG_NAME, name))
+    if sex is not None:
+        individual_element.add_child_element(Element(1, '', tags.GEDCOM_TAG_SEX, sex))
+    if birth_place is not None and birth_date is not None:
+        individual_element.add_child_element(create_event(tags.GEDCOM_TAG_BIRTH, birth_place, birth_date))
+    if death_place is not None and death_date is not None:
+        individual_element.add_child_element(create_event(tags.GEDCOM_TAG_DEATH, death_place, death_date))
+    if family_spouse_ptr is not None:
+        individual_element.add_child_element(Element(1, '', tags.GEDCOM_TAG_FAMILY_SPOUSE, family_spouse_ptr))
+    if family_child_ptr is not None:
+        individual_element.add_child_element(Element(1, '', tags.GEDCOM_TAG_FAMILY_CHILD, family_child_ptr))
+    return individual_element
+
+
+def create_event(tag_type: str, place: str, date: str):
+    event_element = Element(1, '', tag_type, '')
+    event_place_element = Element(2, '', tags.GEDCOM_TAG_PLACE, place)
+    event_element.add_child_element(event_place_element)
+    event_date_element = Element(2, '', tags.GEDCOM_TAG_DATE, date)
+    event_element.add_child_element(event_date_element)
+    return event_element
